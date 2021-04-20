353a019c828fedef5c20132ee03498329579bd6e
webpajooh <webpajooh@gmail.com>
2021-04-20 22:37:21 +0430

    test: create Feature/BookTest

---
+++ tests/Feature/BookTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookTest extends TestCase
+{
+    public function test_we_can_get_the_book_data()
+    {
+        $this->get('/api/book')
+            ->assertJsonStructure([
+                'status',
+                'data' => [
+                    'title',
+                    'author',
+                    'description',
+                    'cover_photo',
+                    'version',
+                    'price',
+                    'free_version_url',
+                ]
+            ]);
+    }
+}
