572c24e13ba0e38153b448c238a6abf5a440120a
webpajooh <webpajooh@gmail.com>
2021-05-14 22:04:09 +0430

    feat: add buy modal

--- resources/js/components/Book.vue
+++ resources/js/components/Book.vue
@@ -29,11 +29,22 @@
 				</div>
 
 				<div class="col-lg-12">
-					<button class="b-button b-button-red fs-17">{{ getPrice }}</button>
-					<button v-if="free_version_url" @click="downloadFreeVersion" class="b-button b-button-gray fs-17">نسخه‌ی نمونه</button>
+					<button @click="showBuyModal" class="b-button b-button-red fs-17 ml-10">{{ getPrice }}</button>
+					<button v-if="free_version_url" @click="downloadFreeVersion" class="b-button b-button-gray fs-17 ml-10">نسخه‌ی نمونه</button>
 				</div>
 			</div>
 		</div>
+
+		<modal name="buy-modal" :adaptive="true">
+			<div class="buy-modal-inner">
+				<center>
+					<img src="/images/email-icon.png">
+				</center>
+
+				<input type="text" class="email-input" placeholder="لطفا آدرس ایمیل خود را وارد کنید">
+				<button class="b-button b-button-red fs-17">تأیید</button>
+			</div>
+		</modal>
 	</div>
 </template>
 
@@ -68,6 +79,10 @@ export default {
 
 		downloadFreeVersion() {
 			window.open(this.free_version_url);
+		},
+
+		showBuyModal() {
+			this.$modal.show('buy-modal')
 		}
 	},
 
@@ -105,47 +120,73 @@ export default {
 		width: 75%;
 		border-radius: 7px;
 	}
+}
 
-	.b-button {
-		float: left;
-		color: #fff;
-		font-weight: bold;
-		border: none;
+.b-button {
+	float: left;
+	color: #fff;
+	font-family: 'FVazir';
+	font-weight: bold;
+	border: none;
+	border-radius: 5px;
+	padding: 12px 19px 9px;
+	cursor: pointer;
+	transition: 0.5s;
+}
+
+.b-button-red {
+	background: #fc5147;
+	box-shadow: 0 3px 6px rgba(232, 59, 49, 0.2);
+
+	&:hover {
+		background: #ff6057;
+		box-shadow: 0 3px 6px rgba(232, 59, 49, 0.4);
+	}
+
+	&:active {
+		background: #e04038;
+		box-shadow: 0 4px 12px rgba(232, 59, 49, 0.4);
+	}
+}
+
+.b-button-gray {
+	background: #646363;
+	box-shadow: 0 3px 6px rgba(87, 87, 87, 0.2);
+
+	&:hover {
+		background: #727272;
+		box-shadow: 0 3px 6px rgba(87, 87, 87, 0.4);
+	}
+
+	&:active {
+		background: #5c5c5c;
+		box-shadow: 0 4px 12px rgba(87, 87, 87, 0.4);
+	}
+}
+
+.buy-modal-inner {
+	padding: 10px 25px;
+
+	img {
+		width: 225px;
+		margin-bottom: 7px;
+	}
+
+	input {
+		width: 80%;
+		font-family: 'FVazir';
+		font-size: 15px;
+		border: 1px solid #d9d9d9;
 		border-radius: 5px;
-		padding: 12px 19px 9px;
-		margin-left: 10px;
-		cursor: pointer;
-		transition: 0.5s;
+		text-align: center;
+		padding: 8px 10px;
 	}
 
-	.b-button-red {
-		background: #fc5147;
-		box-shadow: 0 3px 6px rgba(232, 59, 49, 0.2);
-
-		&:hover {
-			background: #ff6057;
-			box-shadow: 0 3px 6px rgba(232, 59, 49, 0.4);
-		}
-
-		&:active {
-			background: #e04038;
-			box-shadow: 0 4px 12px rgba(232, 59, 49, 0.4);
-		}
-	}
-
-	.b-button-gray {
-		background: #646363;
-		box-shadow: 0 3px 6px rgba(87, 87, 87, 0.2);
-
-		&:hover {
-			background: #727272;
-			box-shadow: 0 3px 6px rgba(87, 87, 87, 0.4);
-		}
-
-		&:active {
-			background: #5c5c5c;
-			box-shadow: 0 4px 12px rgba(87, 87, 87, 0.4);
-		}
+	button {
+		width: 18%;
+		font-size: 16px !important;
+		padding: 10px 0 7px;
+		margin-left: 0 !important;
 	}
 }
 </style>
